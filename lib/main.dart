c5dfa9eba3475d801204d777d9cefb5eda9eb2c2
Akhil Daver <akhildaver@gmail.com>
2024-03-25 21:59:15 +0530

    brachchange

--- lib/main.dart
+++ lib/main.dart
@@ -107,7 +107,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the buttones:',
+              'You have pushed the buttone toooooos:',
             ),
             Text(
               '$_counter',
